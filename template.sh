de9100d35dcef6e815f68495e48d1bdd23770cdf
Jason Park <parkjs814@gmail.com>
2016-05-17 02:53:46 -0500

    fix template.sh for LINUX

--- template.sh
+++ template.sh
@@ -11,8 +11,8 @@ cd $city
 git remote set-url origin https://github.com/IncubateX/${city}.git
 git remote add upstream https://github.com/IncubateX/IncubateTemplate.git
 cp -f _README.md README.md
-sed -i "" "s/{city}/${city}/g" README.md
-sed -i "" "s/{CITY}/${CITY}/g" README.md
+sed -i "s/{city}/${city}/g" README.md
+sed -i "s/{CITY}/${CITY}/g" README.md
 git add .
 git commit -m 'fork from IncubateTemplate'
 git push -u origin master
